370b404f486faa7e24437ceb9c4a24de073cd9ef
zeeshan <zeeshankalimkhan@gmail.com>
2023-05-04 01:15:01 +0530

    learning CRON expression in Spring scheduler

--- src/main/java/com/connect/campus/services/SchedulerServices.java
+++ src/main/java/com/connect/campus/services/SchedulerServices.java
@@ -4,6 +4,8 @@ import com.connect.campus.dao.StudentRepository;
 import com.connect.campus.entities.StudentEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -15,43 +17,67 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
+@EnableAsync
 public class SchedulerServices {
     @Autowired
     StudentRepository studentRepository;
-    @Scheduled(fixedRate = 2000)
-    public void scheduleTesting() throws InterruptedException{
-        LocalDateTime time= LocalDateTime.now();
-        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String formattedTime=time.format(formatter);
-        System.out.println("Scheduler Time: "+ formattedTime);
-        Thread.sleep(5000);
-    }
+//    @Async
+//    @Scheduled(fixedRate = 2000)
+//    public void scheduleTesting() throws InterruptedException{
+//        LocalDateTime time= LocalDateTime.now();
+//        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+//        String formattedTime=time.format(formatter);
+//        System.out.println("Scheduler Time: "+ formattedTime);
+//        Thread.sleep(5000);
+//    }
 
-    @Scheduled(initialDelay = 3,fixedDelay = 2,timeUnit = TimeUnit.SECONDS)
-    public void testingSchedule(){
-        log.info("INSIDE TESTING Schedule");
-    }
+//    @Async
+//    @Scheduled(initialDelay = 3,fixedDelay = 2,timeUnit = TimeUnit.SECONDS)
+//    public void testingSchedule(){
+//        log.info("INSIDE TESTING Schedule");
+//    }
 
-//    public void feesUnpaidMail(){
-//        List<StudentEntity> students=studentRepository.findUnpaidStudents();
-//        List<String> parentEmails= new ArrayList<>();
-//        List<String> studentEmails= new ArrayList<>();
+//    @Scheduled(fixedRateString = "PT12S")
+//    public void test(){
+//        log.info("FixedRate String");
+//    }
+
+//    @Scheduled(cron = "3 */1 * 4 * *")
+//    public void cron(){
+//        log.info("Testing CRON-- method");
+//    }
+
+//    @Scheduled(cron = "${cron.expression.value}")
+//    public void cronThroughApplProp(){
+//        log.info("Getting CRON values from application properties");
+//    }
+
+//    @Scheduled(cron = "@hourly")
+//    public void inBuiltMacros(){
+//        log.info("Using InBuilt macros in CRON");
+//    }
+
+    public void feesUnpaidMail(){
+        List<StudentEntity> students=studentRepository.findUnpaidStudents();
+        List<String> parentEmails= new ArrayList<>();
+        List<String> studentEmails= new ArrayList<>();
+
+        for(StudentEntity student:students){
+            parentEmails.add(student.getParentEmail());
+            studentEmails.add(student.getStudentEmail());
+        }
+
+        System.out.println();
+
+//        //Sending mail to parent
+//        for(String parentMail:parentEmails){
 //
-//        for(StudentEntity student:students){
-//            parentEmails.add(student.getParentEmail());
-//            studentEmails.add(student.getStudentEmail());
 //        }
 //
-//        System.out.println();
+//        //Sending mail to student
+//        for(String studentEmail: studentEmails){
 //
-////        //Sending mail to parent
-////        for(String parentMail:parentEmails){
-////
-////        }
-////
-////        //Sending mail to student
-////        for(String studentEmail: studentEmails){
-////
-////        }
-//    }
+//        }
+    }
+
 }
